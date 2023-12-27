af0e7210238f9f5aa4fa35dfd098596dc37963d9
IvanARashid <ivan52x53@gmail.com>
2023-12-27 09:50:27 +0100

    Modified to allow extension only post 180-pulse

--- common/gwf_tools/fwf_gwf_force_dur.m
+++ common/gwf_tools/fwf_gwf_force_dur.m
@@ -1,23 +1,38 @@
-function [gwf, rf, dt] = fwf_gwf_force_dur(gwf, rf, dt, dur)
+function [gwf, rf, dt] = fwf_gwf_force_dur(gwf, rf, dt, dur, post_only)
 % function [gwf, rf, dt] = fwf_gwf_force_dur(gwf, rf, dt, dur)
 % By FSz
 %
 % Function forces a total duration of the gradient waveform (including
 % padding by zeros) without rescaling the input (just padding).
+%
+% Modification by Ivan A. Rashid 27 Dec 2023
+% post_only : 0 if padding is to be applied symmetrically
+%           : 1 if padding is only to be applied on the post-180 pulse
+
+if nargin < 5
+    post_only = 0;
+end
 
 n1 = sum(rf>0);
 n2 = sum(rf<0);
 
-n  = round(dur/dt/2);
+n = round(dur/dt/2);
 
 p1 = n-n1;
 p2 = n-n2;
 
-if p1 < 0
+if p1 < 0 && post_only == 0
     error
 end
-if p2 < 0
+if p2 < 0 && post_only == 0
     error
+end
+
+% Only pad post-180 pulse
+if post_only == 1
+    n = round(dur/dt);
+    p1 = 0;
+    p2 = n;
 end
 
 gwf = [zeros(p1, 3); gwf];
@@ -27,7 +42,7 @@ gwf = [gwf; zeros(p2,3)];
 rf  = [rf;  rf(end)*ones(p2,1)];
 
 
-if sum(rf)
+if sum(rf) && post_only == 0
     error
 end
 
